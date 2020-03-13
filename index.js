fd449e0798407117b6fc6278ddd2c6964933e8bb
LaNice Powell <lanicemp@aol.com>
2020-03-13 12:56:29 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Your code here
+function saturdayFun(something ="roller-skate") {
+    return(`This Saturday, I want to ${something}!`);
+}
+
+  
+function mondayWork(something ="go to the office") {
+    return(`This Monday, I will ${something}.`);
+}
+
+function wrapAdjective(flair = "*") {
+    return function(something ="special" ){
+        return(`You are ${flair}${something}${flair}!`);
+    }
+}
+  
+const Calculator = { 
+    add: function(a,b){ 
+        return a+b;
+    }, 
+    subtract: function(a,b){
+        return a-b;
+    }, 
+    multiply: function(a,b){
+        return a*b;
+    }, 
+    divide: function(a,b){
+        return a/b;
+    }
+}
+
+function actionApplyer(start, finish){
+    for (let i = 0; i < finish.length; i++){
+        start = finish[i](start);
+    }
+    return start;
+}
